c77161ddedc5fcca95d3490d2499942561334c9b
kaushik <kaushik.t@reportgarden.com>
2017-06-29 00:14:55 +0530

    EOD wednesday

--- es6_2/ques2.js
+++ es6_2/ques2.js
@@ -1,16 +1,29 @@
 const root = document.createElement('div');
 let cur_root = root;
 root.id = 'root'
+document.body.appendChild(root);
+document.getElementById('root_display').innerHTML = `The current root is : ${cur_root.id}`;
 
 function displayList() {
+  // document.getElementById('root_display').innerHTML = `The current root is : ${cur_root.id}`;
   let list_data = cur_root.childNodes;
-  let list = document.createElement('ul')
-  for (let i = 0; i < list_data.length; i++) {
-    let li = document.createElement('li')
-    li.innerHTML = list[i];
-    list.appendChild(li);
+  if (document.getElementById('display_list')) {
+    document.getElementById('list_area').removeChild(document.getElementById('display_list'));
   }
-  document.getElementById('list').appendChild(list);
+  let list = document.createElement('ul')
+  list.id = 'display_list'
+  for (let i = 0; i < list_data.length; i++) {
+    if (list_data[i].id) {
+      let li = document.createElement('li')
+      li.innerHTML = list_data[i].id;
+      if (list_data[i].class === 'folder') {
+        li.addEventListener('click' , setRoot);
+        li.style.color = 'red';
+      }
+      list.appendChild(li);
+    }
+  }
+  document.getElementById('list_area').appendChild(list);
 }
 function generatePath() {
 
@@ -20,6 +33,7 @@ function createForm() {
     let block = document.getElementById('create');
     let f = document.createElement("form");
     f.id = "create_form"
+    f.addEventListener('submit' , getData);
     let input = document.createElement('input');
     input.placeholder = 'Enter filename/foldername';
     input.id = 'create_input'
@@ -27,28 +41,35 @@ function createForm() {
 
     let submit = document.createElement('input');
     submit.type = 'button';
-    submit.value = "Add File/Foler"
+    submit.value = "Add File/Folder"
     submit.id = 'create_submit'
     submit.addEventListener('click' , getData);
     f.appendChild(submit);
     block.appendChild(f);
 };
 createForm();
-function getData() {
+function getData(event) {
+  event.preventDefault();
   let input_name = document.getElementById("create_input").value;
   if (input_name !== '') {
     let temp = input_name.split('.');
     let obj = document.createElement('div');
     obj.class = (temp.length-1)? 'file' : 'folder';
     obj.id = input_name;
-    if (obj.class === 'file') {
-      obj.addEventListener('click' , setRoot);
-    }
     cur_root.appendChild(obj)
+    displayList();
+    document.getElementById("create_input").value = '';
   }
-
 }
 function setRoot() {
-  cur_root = this;
+  cur_root = document.getElementById(this.innerHTML);
+  displayList();
+  document.getElementById('root_display').innerHTML = `The current root is : ${cur_root.id}`;
+}
+function moveUp() {
+  if (cur_root !== root) {
+    cur_root = cur_root.parentNode;
+  }
+  document.getElementById('root_display').innerHTML = `The current root is : ${cur_root.id}`;
   displayList();
 }
